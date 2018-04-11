679d86f6f7e564ff87fb209042b3e8623499324f
Alec DiFederico <alecdifederico@gmail.com>
2018-04-10 22:01:32 -0400

    reorganized starter code

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import 'aframe';
+import 'aframe-animation-component';
+import 'aframe-particle-system-component';
+import 'babel-polyfill';
+import { Entity, Scene } from 'aframe-react';
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {color: 'red'};
+  }
+
+  changeColor() {
+    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
+    this.setState({
+      color: colors[Math.floor(Math.random() * colors.length)]
+    });
+  }
+
+  render() {
+    return (
+      <Scene>
+        // The image assets to use
+        <a-assets>
+          <img id="groundTexture" src="img/floor.jpg"/>
+          <img id="skyTexture" src="img/sky.jpg"/>
+        </a-assets>
+
+        // Scenery entities
+        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
+        <Entity primitive="a-light" type="ambient" color="#445451"/>
+        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
+        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
+        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
+        <Entity text={{value: 'Hello There', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
+
+        // The main box entity
+        <Entity id="box"
+          geometry={{primitive: 'box'}}
+          material={{color: this.state.color, opacity: 0.6}}
+          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
+          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
+          position={{x: 0, y: 1, z: -3}}
+          events={{click: this.changeColor.bind(this)}}>
+          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
+                  geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
+                  material={{color: '#24CAFF'}}/>
+        </Entity>
+
+        // Camera/user entity
+        <Entity primitive="a-camera">
+          <Entity primitive="a-cursor" animation__click={{ property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
+        </Entity>
+      </Scene>
+    );
+  }
+}
+
+export default App;
