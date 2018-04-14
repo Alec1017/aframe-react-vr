225adfce5dc9be98c4ae92d81a69f3c63cbef653
Alec DiFederico <alecdifederico@gmail.com>
2018-04-14 19:33:48 -0400

    added animation

--- src/App.js
+++ src/App.js
@@ -1,52 +1,88 @@
 import 'aframe';
+import 'aframe-effects';
 import 'aframe-animation-component';
 import 'aframe-environment-component';
 import 'aframe-particle-system-component';
-import './components/aframe-lowpoly';
+import 'aframe-event-set-component';
 import 'babel-polyfill';
 import { Entity, Scene } from 'aframe-react';
 import React, { Component } from 'react';
 
+import './components/aframe-lowpoly';
+
 
 // Color palette
-const COLORS =['#D29B6A', '#9564F2', '#FFCF59']
+const COLORS =['#D92B6A', '#9564F2', '#FFCF59']
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       colorIndex: 0,
-      spherePosition: { x: 0.0, y: 4, z: -10 }
+      spherePosition: { x: 3, y: 4, z: -10 }
     };
+  }
+
+  _handleClick() {
+    this.setState({
+      colorIndex: (this.state.colorIndex + 1) % COLORS.length
+    })
   }
 
   render() {
     return (
-      <Scene environment={{
-        preset: 'starry',
-        seed: 2,
-        lightPosition: { x: 10, y: 0.03, z: 0.5 },
-        fog: 0.8,
-        ground: 'canyon',
-        groundYScale: 6.31,
-        groundTexture: 'walkernoise',
-        groundcolor: '#8a7f8a',
-        grid: 'none'
-      }}>
+      <Scene
+        fxaa
+        environment={{
+          preset: 'starry',
+          seed: 2,
+          lightPosition: { x: 10, y: 0.03, z: 0.5 },
+          fog: 0.8,
+          ground: 'canyon',
+          groundYScale: 6.31,
+          groundTexture: 'walkernoise',
+          groundcolor: '#8a7f8a',
+          grid: 'none'
+        }}
+      >
 
         // Low-poly entity
         <Entity
+          class="clickable"
           lowpoly={{
-            color: '#D92B6A',
+            color: COLORS[this.state.colorIndex],
             nodes: true,
             opacity: 0.15,
             wireframe: true
           }}
+          animation__rotate={{
+            property: 'rotation',
+            dur: 60000,
+            easing: 'linear',
+            loop: true,
+            to: { x: 0, y: -360, z: 0 }
+          }}
+          animation__oscillate={{
+            property: 'position',
+            dur: 2000,
+            dir: 'alternate',
+            easing: 'linear',
+            loop: true,
+            from: this.state.spherePosition,
+            to: {
+              x: this.state.spherePosition.x,
+              y: this.state.spherePosition.y + 0.2,
+              z: this.state.spherePosition.z
+            }
+          }}
           primitive="a-octahedron"
           detail={2}
           radius={2}
-          position={{ x: 0.0, y: 4, z: -10.0 }}
-          color="FAFAFA1"
+          position={this.state.spherePosition}
+          color="#FAFAF1"
+          events={{
+            click: this._handleClick.bind(this)
+          }}
         />
 
         // Lighting
@@ -65,6 +101,13 @@ class App extends Component {
             cursor={{ fuse: false }}
             material={{ color: 'white', shader: 'flat', opacity: 0.75 }}
             geometry={{ radiusInner: 0.005, radiusOuter: 0.007 }}
+            event-set__mouseenter={{
+              scale: { x: 1.4, y: 1.4, z: 1.4 }
+            }}
+            event-set__mouseleave={{
+              scale: { x: 1, y: 1, z: 1 }
+            }}
+            raycaster="objects: .clickable"
           />
         </Entity>
       </Scene>
--- src/components/aframe-lowpoly.js
+++ src/components/aframe-lowpoly.js
@@ -16,6 +16,9 @@ AFRAME.registerComponent('lowpoly', {
     // Get the reference to the scene
     const scene = document.querySelector('a-scene').object3D;
 
+    // Get the position of the object to which the component is attached
+    const { x, y, z } = this.el.getAttribute('position');
+
     // Modify the color of the material
     obj.material = new THREE.MeshPhongMaterial({
       color: this.data.color,
@@ -24,7 +27,8 @@ AFRAME.registerComponent('lowpoly', {
 
     // Wireframe segments initialization
     // Define geometry and material for the wireframe segments
-    const wireGeom = new THREE.OctahedronGeometry(2.5, 2);
+    const objRadius = Number(this.el.getAttribute('radius'));
+    const wireGeom = new THREE.OctahedronGeometry(objRadius + .5, 2);
     const wireMaterial = new THREE.MeshPhongMaterial({
       color: '#FFFFFF',
       opacity: this.data.opacity,
@@ -36,8 +40,7 @@ AFRAME.registerComponent('lowpoly', {
     const wireFrame = new THREE.Mesh(wireGeom, wireMaterial);
 
     // Set position of the mesh to the position of the sphere
-    const { x, y, z} = obj.position;
-    wireFrame.position.set(0.0, 4, -10.0);
+    wireFrame.position.set(x, y, z);
 
     // Add wireframe to the scene
     if (this.data.wireframe) {
@@ -54,7 +57,7 @@ AFRAME.registerComponent('lowpoly', {
         // Define geometry and material for a wireframe node
         let geometry = new THREE.SphereGeometry(0.045, 16, 16);
         let material = new THREE.MeshBasicMaterial({
-          color: '#FFFFF',
+          color: '#FFFFFF',
           opacity: this.data.opacity,
           shading: THREE.FlatShading,
           transparent: true
@@ -65,9 +68,9 @@ AFRAME.registerComponent('lowpoly', {
 
         // Set position of the spheres
         sphere.position.set(
-          vertices[i].x,
-          vertices[i].y + 4,
-          vertices[i].z + -10.0
+          vertices[i].x + x,
+          vertices[i].y + y,
+          vertices[i].z + z
         );
 
         // Add sphere to the group
@@ -77,5 +80,14 @@ AFRAME.registerComponent('lowpoly', {
       // Add spheres to the scene
       scene.add(spheres);
     }
+  },
+
+
+  update: function() {
+    // Get the reference of the object to which the component is attached
+    const obj = this.el.getObject3D('mesh');
+
+    // Modify the color of the material during runtime
+    obj.material.color = new THREE.Color(this.data.color);
   }
 });
